24a13230389a2a1b4f43a2014b249a1e3447ce2d
Clifton Toaster Reid <42320331+cliftontoaster-reid@users.noreply.github.com>
2024-07-03 12:42:45 +0200

    Refactor `get_request` method for reusability
    Refactored the get_request method to improve code reusability.
    
    - Extracted common logic for `GET` and `POST` requests
    - Added error handling for `URL` parsing
    - Updated `get_mod_info` and `get_mod_info_full` methods to use `get_request`
    - Improved code readability and maintainability

--- furrctorio_core/src/model/context.rs
+++ furrctorio_core/src/model/context.rs
@@ -26,24 +26,34 @@ impl Context {
   ///
   /// * `Result<Self, reqwest::Error>` - Returns a Result containing the created Context instance or a reqwest::Error.
   #[instrument]
-  pub async fn new(username: String, password: String, email_code: Option<String>) -> Result<Self, reqwest::Error> {
-    let req_url = Url::parse_with_params("https://auth.factorio.com/api-login",
-    &[
-      ("username", encode(&username).to_string().as_str()),
-      ("password", encode(&password).to_string().as_str()),
-      ("api_version", "4"),
-      ("require_game_ownership", "true"),
-    ]).unwrap();
+  pub async fn new(
+    username: String,
+    password: String,
+    email_code: Option<String>,
+  ) -> Result<Self, reqwest::Error> {
+    let req_url = Url::parse_with_params(
+      "https://auth.factorio.com/api-login",
+      &[
+        ("username", encode(&username).to_string().as_str()),
+        ("password", encode(&password).to_string().as_str()),
+        ("api_version", "4"),
+        ("require_game_ownership", "true"),
+      ],
+    )
+    .unwrap();
     debug!("{}", req_url);
 
-    let req = reqwest::Client::new()
-      .request(Method::POST, req_url);
+    let req = reqwest::Client::new().request(Method::POST, req_url);
 
     let code: Value = if let Some(code) = email_code {
       req.header("email_code", code)
     } else {
       req
-    }.send().await?.json().await?;
+    }
+    .send()
+    .await?
+    .json()
+    .await?;
 
     if let Ok(token) = from_value::<Vec<String>>(code.clone()) {
       return Ok(Context {
@@ -51,10 +61,42 @@ impl Context {
         token: token.first().unwrap().clone(),
       });
     } else if let Ok(ctx) = from_value::<Context>(code.clone()) {
-      return Ok(ctx)
+      return Ok(ctx);
     } else {
       panic!("Coud not login.")
     }
+  }
+
+  /// Creates a new request builder with the specified method and URL.
+  ///
+  /// # Arguments
+  ///
+  /// * `method` - The HTTP method for the request.
+  /// * `url` - The URL for the request.
+  /// * `auth` - A boolean indicating whether authentication is required.
+  ///
+  /// # Returns
+  ///
+  /// * `Result<reqwest::RequestBuilder, url::ParseError>` - Returns a Result containing the request builder or a url::ParseError.
+  fn get_request(
+    &self,
+    method: Method,
+    url: &str,
+    auth: bool,
+  ) -> Result<reqwest::RequestBuilder, url::ParseError> {
+    let req_url = if auth {
+      // If authentication is required, parse the URL with username and token as parameters.
+      Url::parse_with_params(url, &[("username", &self.username), ("token", &self.token)])?
+    } else {
+      // If authentication is not required, parse the URL as is.
+      Url::parse(url)?
+    };
+
+    // Create a new request builder with the specified method and URL.
+    let base = reqwest::Client::new().request(method, req_url);
+
+    // Return the request builder.
+    Ok(base)
   }
 
   /// Fetches short information about a mod from the Factorio mods server.
@@ -67,9 +109,17 @@ impl Context {
   ///
   /// * `Result<FModShort, reqwest::Error>` - Returns a Result containing the short mod information or a reqwest::Error.
   pub async fn get_mod_info(&self, mod_name: &str) -> Result<FModShort, reqwest::Error> {
-    reqwest::Client::new()
-      .request(Method::GET, format!("https://mods.factorio.com/api/mods/{}", encode(mod_name)))
-      .send().await.unwrap().json().await
+  self
+      .get_request(
+        Method::GET,
+        &format!("https://mods.factorio.com/api/mods/{}", encode(mod_name)),
+        false,
+      )
+      .unwrap()
+      .send()
+      .await?
+      .json()
+      .await
   }
 
   /// Fetches full information about a mod from the Factorio mods server.
@@ -82,8 +132,20 @@ impl Context {
   ///
   /// * `Result<FModFull, reqwest::Error>` - Returns a Result containing the full mod information or a reqwest::Error.
   pub async fn get_mod_info_full(&self, mod_name: &str) -> Result<FModFull, reqwest::Error> {
-    reqwest::Client::new()
-      .request(Method::GET, format!("https://mods.factorio.com/api/mods/{}/full", encode(mod_name)))
-      .send().await.unwrap().json().await
+    self
+      .get_request(
+        Method::GET,
+        &format!(
+          "https://mods.factorio.com/api/mods/{}/full",
+          encode(mod_name)
+        ),
+        false,
+      )
+      .unwrap()
+      .send()
+      .await
+      .unwrap()
+      .json()
+      .await
   }
 }
